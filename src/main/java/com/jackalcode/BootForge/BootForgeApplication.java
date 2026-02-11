5cef195f1d19ec31e7b9e8aed14100248515081f
ernest-nnodu <ernest.nnodu@outlook.com>
2026-02-11 18:46:22 +0000

    Initial commit

---
+++ src/main/java/com/jackalcode/BootForge/BootForgeApplication.java
@@ -0,0 +1,14 @@
+package com.jackalcode.BootForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootForgeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BootForgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackalcode/BootForge/BootForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackalcode.BootForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
