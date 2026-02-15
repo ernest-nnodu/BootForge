57ffe8911c719160243b11cf80c903544fb80319
ernest-nnodu <ernest.nnodu@outlook.com>
2026-02-15 17:17:06 +0000

    Add config formatter interface

---
+++ src/main/java/com/jackalcode/BootForge/formatter/ConfigFormatter.java
@@ -0,0 +1,8 @@
+package com.jackalcode.BootForge.formatter;
+
+import com.jackalcode.BootForge.domain.model.Configuration;
+
+public interface ConfigFormatter {
+
+    String format(Configuration configuration);
+}
