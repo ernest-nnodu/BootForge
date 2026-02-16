6363c890d049b89d8078a16d9a962e425445a0fa
ernest-nnodu <ernest.nnodu@outlook.com>
2026-02-16 21:35:18 +0000

    Implement format application config and format database config method

--- src/main/java/com/jackalcode/BootForge/formatter/YamlFormatter.java
+++ src/main/java/com/jackalcode/BootForge/formatter/YamlFormatter.java
@@ -1,11 +1,61 @@
 package com.jackalcode.BootForge.formatter;
 
+import com.jackalcode.BootForge.domain.model.ApplicationConfig;
 import com.jackalcode.BootForge.domain.model.Configuration;
+import com.jackalcode.BootForge.domain.model.DatabaseConfig;
+import com.jackalcode.BootForge.formatter.util.FormatterUtil;
 
 public class YamlFormatter implements ConfigFormatter {
 
     @Override
     public String format(Configuration configuration) {
-        return "";
+
+        return """
+                spring:
+                  %s
+                 \s
+                  %s
+               \s"""
+                .formatted(
+                        formatApplicationConfig(configuration.applicationConfig()),
+                        formatDatabaseConfig(configuration.databaseConfig())
+                );
+    }
+
+    private String formatApplicationConfig(ApplicationConfig applicationConfig) {
+
+        return """
+                  application:
+                    name:%s
+                   \s
+                  profiles:
+                    active:%s
+                   \s
+               \s"""
+                .formatted(
+                        applicationConfig.applicationName(),
+                        applicationConfig.activeProfile()
+                );
+    }
+
+    private String formatDatabaseConfig(DatabaseConfig databaseConfig) {
+
+        return """
+                  datasource:
+                    url:%s
+                    username:%s
+                    password:%s
+                   \s
+               \s"""
+                .formatted(
+                        FormatterUtil.generateDatasourceUrl(
+                                databaseConfig.databaseType(),
+                                databaseConfig.databaseName(),
+                                databaseConfig.host(),
+                                databaseConfig.port()
+                        ),
+                        databaseConfig.username(),
+                        databaseConfig.password()
+                );
     }
 }
