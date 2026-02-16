ff7912661ec13dea79f9b012d6d60269f0523f21
ernest-nnodu <ernest.nnodu@outlook.com>
2026-02-16 21:09:07 +0000

    Add yaml formatter

---
+++ src/main/java/com/jackalcode/BootForge/formatter/YamlFormatter.java
@@ -0,0 +1,11 @@
+package com.jackalcode.BootForge.formatter;
+
+import com.jackalcode.BootForge.domain.model.Configuration;
+
+public class YamlFormatter implements ConfigFormatter {
+
+    @Override
+    public String format(Configuration configuration) {
+        return "";
+    }
+}
