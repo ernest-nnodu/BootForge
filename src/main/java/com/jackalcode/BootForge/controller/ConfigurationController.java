7b6a5284f46f29fd0d6de26bbb3fd27a6361601f
ernest-nnodu <ernest.nnodu@outlook.com>
2026-02-18 17:24:03 +0000

    Implement generate config method in configuration method

--- src/main/java/com/jackalcode/BootForge/controller/ConfigurationController.java
+++ src/main/java/com/jackalcode/BootForge/controller/ConfigurationController.java
@@ -1,6 +1,12 @@
 package com.jackalcode.BootForge.controller;
 
+import com.jackalcode.BootForge.dto.GenerateConfigRequest;
 import com.jackalcode.BootForge.service.ConfigurationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +16,13 @@ public class ConfigurationController {
 
     public ConfigurationController(ConfigurationService configurationService) {
         this.configurationService = configurationService;
+    }
+
+    @PostMapping(path = "/generate")
+    public ResponseEntity<String> generateConfig(
+            @RequestBody @Valid GenerateConfigRequest configRequest) {
+
+        String configResponse = configurationService.generateConfiguration(configRequest);
+        return new ResponseEntity<>(configResponse, HttpStatus.OK);
     }
 }
