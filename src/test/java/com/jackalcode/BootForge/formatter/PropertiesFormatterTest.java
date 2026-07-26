a71e284026df64d2833ed70d08593fa251f5a65b
ernest-nnodu <ernest.nnodu@outlook.com>
2026-07-26 11:43:17 +0100

    Add properties formatter test.

---
+++ src/test/java/com/jackalcode/BootForge/formatter/PropertiesFormatterTest.java
@@ -0,0 +1,13 @@
+package com.jackalcode.BootForge.formatter;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class PropertiesFormatterTest {
+
+    private PropertiesFormatter propertiesFormatter;
+
+    @BeforeEach
+    void setUp() {
+        propertiesFormatter = new PropertiesFormatter();
+    }
+}
