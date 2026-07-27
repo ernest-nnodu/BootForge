abe516cd5be89ee9961409acf9e63d85aa194ca9
ernest-nnodu <ernest.nnodu@outlook.com>
2026-07-27 22:04:00 +0100

    Add unit tests to validate properties format is generated when output format is properties.

--- src/test/java/com/jackalcode/BootForge/service/ConfigurationServiceTest.java
+++ src/test/java/com/jackalcode/BootForge/service/ConfigurationServiceTest.java
@@ -1,17 +1,75 @@
 package com.jackalcode.BootForge.service;
 
+import com.jackalcode.BootForge.common.ConfigurationTestHelper;
+import com.jackalcode.BootForge.common.GenerateConfigRequestTestHelper;
+import com.jackalcode.BootForge.common.RequestProps;
+import com.jackalcode.BootForge.domain.enums.*;
 import com.jackalcode.BootForge.formatter.PropertiesFormatter;
 import com.jackalcode.BootForge.formatter.YamlFormatter;
-import org.junit.jupiter.api.BeforeEach;
+import com.jackalcode.BootForge.mapper.ConfigurationMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class ConfigurationServiceTest {
 
+
+    @Mock
     private PropertiesFormatter propertiesFormatter;
+
+    @Mock
     private YamlFormatter yamlFormatter;
 
-    @BeforeEach
-    void setUp() {
-        propertiesFormatter = new PropertiesFormatter();
-        yamlFormatter = new YamlFormatter();
+    @Mock
+    private ConfigurationMapper configurationMapper;
+
+    @InjectMocks
+    private ConfigurationService configurationService;
+
+    @Test
+    @DisplayName("generateConfiguration generates properties configuration when output format is properties")
+    void generateConfiguration_whenOutputFormatIsProperties_shouldReturnProperties() {
+
+        var requestProps = RequestProps.builder()
+                .applicationName("test-app")
+                .serverPort(8080)
+                .databaseName("test-db")
+                .username("test-user")
+                .password("password")
+                .databaseType(DatabaseType.POSTGRESQL)
+                .databasePort(5555)
+                .outputFormat(OutputFormat.PROPERTIES)
+                .build();
+        var configRequest = GenerateConfigRequestTestHelper.generateConfigRequest(requestProps);
+        var configuration = ConfigurationTestHelper.toConfiguration(configRequest);
+
+        String expectedOutput = """
+                spring.application.name=boot-forge
+                server.port=8080
+                """;
+
+        when(configurationMapper.toConfiguration(configRequest))
+                .thenReturn(configuration);
+
+        when(propertiesFormatter.format(configuration))
+                .thenReturn(expectedOutput);
+
+        String result = configurationService.generateConfiguration(configRequest);
+
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(expectedOutput);
+
+        verify(configurationMapper).toConfiguration(configRequest);
+        verify(propertiesFormatter).format(configuration);
+        verifyNoInteractions(yamlFormatter);
+
+
     }
 }
