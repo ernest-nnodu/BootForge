961e1fd54196897a46fd68118f05ee16b1aa24da
ernest-nnodu <ernest.nnodu@outlook.com>
2026-09-24 22:24:10 +0100

    Add OpenAPI annotations for DatabaseType enum

--- src/main/java/com/jackalcode/BootForge/domain/enums/DatabaseType.java
+++ src/main/java/com/jackalcode/BootForge/domain/enums/DatabaseType.java
@@ -1,5 +1,8 @@
 package com.jackalcode.BootForge.domain.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Supported database types for datasource configuration")
 public enum DatabaseType {
     POSTGRESQL,
     MYSQL
